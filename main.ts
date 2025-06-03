0def6aa2f6f75f0dae47392b021ef49029fd992e
Louna86 <lounafrey@gmx.fr>
2025-06-03 15:21:52 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,14 @@ enum RadioMessage {
     CONFINEMENT = 15186,
     INCENDIE = 7432
 }
+radio.onReceivedMessage(RadioMessage.CONFINEMENT, function () {
+    for (let index = 0; index < 100; index++) {
+        music.setVolume(150)
+        basic.showString("CONFINEMENT")
+        music.play(music.stringPlayable("G E G E G E G E ", 180), music.PlaybackMode.UntilDone)
+        basic.showString("SE DIRIGER DANS DES SALLES CLOSES A L'ETAGE")
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendMessage(RadioMessage.INCENDIE)
     for (let index = 0; index < 100; index++) {
@@ -11,7 +19,6 @@ input.onButtonPressed(Button.A, function () {
         basic.showString("FEU")
         music.play(music.stringPlayable("C5 A C5 A C5 A C5 A ", 225), music.PlaybackMode.LoopingInBackground)
         basic.showString("→ POINT DE RASSEMBLEMENT")
-        basic.showString("")
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -31,4 +38,24 @@ input.onButtonPressed(Button.B, function () {
         music.play(music.stringPlayable("C F C F C F C F ", 200), music.PlaybackMode.UntilDone)
         basic.showString("SE CACHER SOUS LES TABLES. SILENCE COMPLET")
     }
+})
+radio.onReceivedMessage(RadioMessage.INCENDIE, function () {
+    for (let index = 0; index < 100; index++) {
+        music.setVolume(150)
+        basic.showString("FEU")
+        music.play(music.stringPlayable("C5 A C5 A C5 A C5 A ", 225), music.PlaybackMode.LoopingInBackground)
+        basic.showString("→ POINT DE RASSEMBLEMENT")
+    }
+})
+radio.onReceivedMessage(RadioMessage.INTRUSION, function () {
+    for (let index = 0; index < 100; index++) {
+        basic.showString("INTRUSION")
+        music.setVolume(150)
+        music.play(music.stringPlayable("C F C F C F C F ", 200), music.PlaybackMode.UntilDone)
+        basic.showString("SE CACHER SOUS LES TABLES. SILENCE COMPLET")
+    }
+})
+basic.forever(function () {
+    radio.setGroup(1)
+    radio.sendNumber(0)
 })
